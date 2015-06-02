10eea1d5203f75fc512bffc6cca0cc44af7b9ea3
fupeng <fupeng_2005@126.com>
2015-06-02 14:57:28 +0800

    Update example.php

--- php/example.php
+++ php/example.php
@@ -5,3 +5,14 @@
         $url = urldecode( $this->input->get_post('url'));
         header("Location: $url");
     }    
+
+    #输出json
+    header("Content-type: text/json");
+    echo json_encode($obj);
+    
+    #打印日志函数
+    function LogRecorder($log, $FileName)
+    {        
+        $log = sprintf("%s %s\n", date('Y-m-d H:i:s'), $log);
+        file_put_contents($FileName, $log, FILE_APPEND);
+    }
