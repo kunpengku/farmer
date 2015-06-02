a4afd58079891e4320fa04abea5e42cad542b66d
fupeng <fupeng_2005@126.com>
2015-06-02 15:09:20 +0800

    Update curl.php

--- php/curl.php
+++ php/curl.php
@@ -10,3 +10,21 @@
   curl_close($ch);
   $out_content = json_decode($output);
   $ret = $out_content->retcode;
+  
+  
+<span style="font-size:18px;">$url = "http://www.xxxx.com?user=111";  
+$post_data = "somedata";  
+  
+$ch = curl_init();  
+  
+curl_setopt($ch, CURLOPT_URL, $url);  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+curl_setopt($ch, CURLOPT_HEADER, 0);  
+curl_setopt($ch , CURLOPT_POST , 1 ) ;  
+curl_setopt($ch , CURLOPT_POSTFIELDS , $post_data ) ;  
+  
+$out = curl_exec($ch);  
+curl_close($ch);  
+$obj = json_decode($out);  
+print $obj;  
+</span>
