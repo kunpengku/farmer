0fa3a2e2de6df1fbda2c773058863abff6bdb0a0
fupeng <fupeng_2005@126.com>
2015-06-25 18:17:27 +0800

    Create multiprecess_test1.py

---
+++ python/multiprecess_test1.py
@@ -0,0 +1,38 @@
+# Similarity and difference of multi thread vs. multi process
+# Written by Vamei
+
+import os
+import threading
+import multiprocessing
+
+# worker function
+def worker(sign, lock):
+    lock.acquire()
+    print  sign
+    print(sign, os.getpid())
+    lock.release()
+
+# Main
+print('Main:',os.getpid())
+
+# Multi-thread
+record = []
+lock  = threading.Lock()
+for i in range(5):
+    thread = threading.Thread(target=worker,args=('thread - fu1',lock))
+    thread.start()
+    record.append(thread)
+
+for thread in record:
+    thread.join()
+
+# Multi-process
+record = []
+lock = multiprocessing.Lock()
+for i in range(5):
+    process = multiprocessing.Process(target=worker,args=('fu 2- process',lock))
+    process.start()
+    record.append(process)
+
+for process in record:
+    process.join()
