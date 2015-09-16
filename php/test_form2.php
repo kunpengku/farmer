223a7782d717cda9ffbef3f8eaa00f8b375d51bd
fupeng <fupeng_2005@126.com>
2015-09-17 02:19:58 +0800

    Update test_form2.php

--- php/test_form2.php
+++ php/test_form2.php
@@ -1,3 +1,5 @@
+<?php
+
 if( isset($_POST['submit']) && $_POST['submit'] == '提交' )
 {
     $uname = $_POST['username'];
@@ -11,3 +13,6 @@ print '<form name="form" method="post" >';
 print 'username:<input type="text" name="username" ><br />';
 print '<input type="submit" name="submit" value="提交" >';
 print '</form>';
+
+
+?>
