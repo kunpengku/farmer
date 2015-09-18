176b511421ee0b87b20a8c657fd53ca6bb46b4e1
fupeng <fupeng_2005@126.com>
2015-09-18 12:56:49 +0800

    Create test_sizeof.cpp

---
+++ cpp/test_sizeof.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+	
+	cout << "int" << sizeof(int) << endl;
+	cout << "double" << sizeof(double) << endl;
+	cout << "float" << sizeof(float) << endl;
+	cout << "size_t" << sizeof(size_t) << endl;
+
+	return 0;
+}
