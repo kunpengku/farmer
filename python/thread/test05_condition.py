d6afb8c652d40f25f16b22b778ae51bb2487b52b
fupeng <fupeng_2005@126.com>
2016-01-21 20:20:27 +0800

    Create test05_condition.py

---
+++ python/thread/test05_condition.py
@@ -0,0 +1,53 @@
+# encoding: UTF-8
+import threading
+import time
+ 
+# 商品
+product = None
+# 条件变量
+con = threading.Condition()
+ 
+# 生产者方法
+def produce():
+    global product
+    
+    if con.acquire():
+        while True:
+            if product is None:
+                print 'produce...',threading.currentThread().getName()
+                product = 'anything'
+    
+                # 通知消费者，商品已经生产
+                print '生产好了',threading.currentThread().getName()
+                con.notify()
+                print '生产通知完成',threading.currentThread().getName()
+    
+            # 等待通知
+            con.wait()
+            print 'after produce wait',threading.currentThread().getName()
+            time.sleep(2)
+ 
+# 消费者方法
+def consume():
+    global product
+    
+    if con.acquire():
+        while True:
+            if product is not None:
+                print 'consume...',threading.currentThread().getName()
+                product = None
+    
+                # 通知生产者，商品已经没了
+                print '商品已经没了'   ,threading.currentThread().getName()
+                con.notify()
+                print '消费通知完成',threading.currentThread().getName()
+    
+            # 等待通知
+            con.wait()
+            print 'consumer produce wait',threading.currentThread().getName()
+            time.sleep(2)
+ 
+t1 = threading.Thread(target=produce)
+t2 = threading.Thread(target=consume)
+t2.start()
+t1.start()
