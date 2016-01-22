6eccb054ef0209eecbabb7c3b10070eb2e049c29
fupeng <fupeng_2005@126.com>
2016-01-22 16:25:05 +0800

    Update test_mysql.py

--- python/test_mysql.py
+++ python/test_mysql.py
@@ -1,6 +1,9 @@
 #!/usr/local/bin/python
 #  coding:utf-8 
 import MySQLdb
+import sys 
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 db_handle = MySQLdb.connect(host = '10.10.1.1', user = 'work', passwd = '111111',  db = 'test',port = 3306, charset = 'utf8')      
 
@@ -11,6 +14,8 @@ cur = db_handle.cursor( MySQLdb.cursors.DictCursor )
 cur.execute( sql )
 cur.close()
 db_handle.commit()
+
+chapter_name = MySQLdb.escape_string( self.chapter_name(row['chapter_id']) )
 
 
 
