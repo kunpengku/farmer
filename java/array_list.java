b032303327c307e3f1094d530e0fd56344be7409
fupeng <fupeng_2005@126.com>
2016-11-17 10:11:42 +0800

    Create array_list.java

---
+++ java/array_list.java
@@ -0,0 +1,23 @@
+       
+       
+       ArrayList es_type = new ArrayList();
+        if(res_type == null){
+            es_type.add("courseware");
+            es_type.add("exercise");
+            es_type.add("clip");
+        }else if(res_type.equals("1")){
+            es_type.add("courseware");
+        }else if(res_type.equals("2")){
+            es_type.add("exercise");
+        }else if(res_type.equals("3")){
+            es_type.add("clip");
+        }else if(res_type.equals("4")){
+            es_type.add("clip");
+        }else if(res_type.equals("5")){
+            es_type.add("clip");
+        }else if(res_type.equals("6")){
+            es_type.add("clip");
+        }
+        
+        
+        ====
