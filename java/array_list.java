af3e8cdd933f2f379f4920a66433c8577352f677
fupeng <fupeng_2005@126.com>
2016-11-17 11:13:12 +0800

    Update array_list.java

--- java/array_list.java
+++ java/array_list.java
@@ -1,25 +1,25 @@
        
-       
+       ====
+       初始化ArrayList
        ArrayList es_type = new ArrayList();
-        if(res_type == null){
-            es_type.add("courseware");
-            es_type.add("exercise");
-            es_type.add("clip");
-        }else if(res_type.equals("1")){
-            es_type.add("courseware");
-        }else if(res_type.equals("2")){
-            es_type.add("exercise");
-        }else if(res_type.equals("3")){
-            es_type.add("clip");
-        }else if(res_type.equals("4")){
-            es_type.add("clip");
-        }else if(res_type.equals("5")){
-            es_type.add("clip");
-        }else if(res_type.equals("6")){
-            es_type.add("clip");
-        }
+       添加元素
+       es_type.add("courseware");
+       es_type.add("exercise");
+       es_type.add("clip");
+       
         
         
         ====
-        ArrayList => String []
+        ArrayList 转换数组 String []
+        方法1
         String [] t = (String [])es_type.toArray(new String[0]);
+
+        方法2
+        String [] indice = new String[es_type.size()];
+        int i=0;
+        for (String estype:es_type)
+        {
+            indice[i++] = "teacher-center-"+estype;
+        }
+
+        ====
