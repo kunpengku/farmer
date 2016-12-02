fbc75ac30083389b400755873d77f2b4a5456e5b
fupeng <fupeng_2005@126.com>
2016-12-02 11:40:29 +0800

    Create http_get_test.java

---
+++ java/http_get_test.java
@@ -0,0 +1,47 @@
+public static String sendGet(String url, String param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            //String urlNameString = url + "?" + param;
+            String urlNameString = "http://.com/api/interface/getsubject?word=%E8%AF%BE%E4%BB%B6&zone=31&subject=s2&stype=2&pn=1&rn=30&host=xdf.baidu.com&token=3e3c0dde030a137d7b55960f4c1105ab|1480643982&order=0";
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+
+        System.out.println(result);
+        return result;
+    }
