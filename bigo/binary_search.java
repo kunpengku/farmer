bd81f218e5117860f4c0ee292ad3c53f60540d5d
fupeng <fupeng_2005@126.com>
2016-12-29 18:52:50 +0800

    Update binary_search.java

--- bigo/binary_search.java
+++ bigo/binary_search.java
@@ -1,97 +1,33 @@
-package algorithm;
+/**
+ * Created by fupeng on 2016/12/29.
+ */
+public class BinarySearch {
 
-import java.util.Arrays;
+    static int  binary_search(int [] tmp, int k){
+        int l = 0;
+        int r = tmp.length-1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(k < tmp[mid]){
+                r = mid-1;
+            }else if(k > tmp[mid]){
+                l = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
 
-public class Test {
-	
-	//recursion 
-	static long get_gcd(long p, long q){
-		if (q > p){
-			long tmp = q;
-			q=p;
-			p=tmp;
-		}
-		
-		if (q==0){
-			return p;
-		}
-		
-		long leav = p % q;
-		return get_gcd(leav, q);
-		
-	}
-	
-	 long get_gcd2(long p, long q){
-		
-		long r =0;
-		if (q > p){
-			long tmp = q;
-			q=p;
-			p=tmp;
-		}
-		
-		
-		if (q==0){
-			return p;
-		}
-		
-		
-		while(q != 0){
-			r = p%q;
-			p=q;
-			q=r;
-		}
-		
-		return p;
-		
-	
-		
-	}
-	
-	static boolean isPrime(int p){
-		if (p < 2){
-			return true;
-		}
-		for(int i=2; i*i <= p;i++){
-			if(p%i == 0){
-				return false;
-			}
-		}
-		return true;
-	}
-	 
-	static int rank(int [] list, int key){
-		int left = 0;
-		int right = list.length-1;
+    public static void main(String [] argv){
+        System.out.println("123");
+        int [] tmp = {1, 25  ,59, 60, 92, 145};
 
-		while(left <= right){
-			
-			int mid = left + (right - left)/2;
-			
-			if(list[mid] == key){
-				return mid;
-			}else if(key < list[mid]){
-				right = mid - 1;
-			}else if(key > list[mid]){
-				left = mid + 1;
-			}
-		}
-		
-		return -1;
-	
-	}
-	
-	public static void main(String[] args) {
-		
-		int [] whitelist = {1,3,50,99,13};
-		Arrays.sort(whitelist);
-		
-
-		int key = 99;
-		int ret = rank(whitelist, key);
-		
-		System.out.println(ret);
-		
-		
-	}
+        int ret = binary_search(tmp, 92);
+        if(ret < 0) {
+            System.out.println("不存在");
+        }else{
+            System.out.println("存在");
+        }
+    }
 }
